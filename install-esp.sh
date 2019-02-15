b90b40c8f5c84b8e917664d2b808f84ae2577c90
Dan Leslie <dan@ironoxide.ca>
2019-02-15 10:34:52 -0800

    Cleaned up esp-idf install script

--- install-esp.sh
+++ install-esp.sh
@@ -1,24 +1,36 @@
 #! /bin/bash
 
-sudo usermod -a -G dialout $USER &&							\
-    sudo apt update &&									\
-    sudo apt install -yy libncurses5 git flex bison gperf ffmpeg &&				\
-    wget https://bootstrap.pypa.io/get-pip.py &&						\
-    sudo -H python get-pip.py &&								\
-    sudo -H pip install pyserial &&								\
-    rm get-pip.py &&									\
-    mkdir -p ~/Arduino/hardware/espressif &&						\
-    cd ~/Arduino/hardware/espressif &&							\
-    git clone https://github.com/espressif/arduino-esp32.git esp32 &&			\
-    cd esp32 &&										\
-    git submodule update --init --recursive &&						\
-    cd tools &&										\
-    python3 get.py &&									\
-    cd ~ &&											\
-    git clone https://github.com/hardkernel/ODROID-GO.git ~/Arduino/libraries/ODROID-GO &&	\
-    mkdir -p ~/esp/esp-idf-odroidgo &&							\
-    git clone https://github.com/OtherCrashOverride/esp-idf --recursive ~/esp/esp-idf-odroid-go &&	\
-    cd ~/esp/esp-idf-odroidgo/ && \
-    git checkout release/v3.2-odroidgo && \
-    python2 -m pip install --user -r ~/esp/esp-idf-odroidgo/requirements.txt &&             \
-    echo "Now you'll want to point IDF_PATH at ~/esp/esp-idf-odroidgo/"    
+local python=python3
+local pip=pip
+
+mkdir -p ~/Arduino/libraries/
+mkdir -p ~/Arduino/hardware/
+
+sudo usermod -a -G dialout $USER
+sudo apt update &&
+    sudo apt install -yy libncurses5 git flex bison gperf ffmpeg
+
+git clone https://github.com/hardkernel/ODROID-GO.git ~/Arduino/libraries/ODROID-GO
+
+local tmpdir=`mktemp -d`
+cd $tmpdir &&
+    wget https://bootstrap.pypa.io/get-pip.py &&
+    sudo -H $python get-pip.py &&
+    sudo -H $pip install pyserial
+sudo rm -rf $tmpdir
+
+mkdir -p ~/Arduino/hardware/espressif
+cd ~/Arduino/hardware/espressif &&
+    git clone https://github.com/espressif/arduino-esp32.git esp32
+
+cd ~/Arduino/hardware/espressif/esp32 &&
+    git submodule update --init --recursive
+
+cd ~/Arduino/hardware/espressif/esp32/tools &&
+    $python get.py
+
+mkdir -p ~/esp/
+cd ~/esp &&
+    git clone https://github.com/OtherCrashOverride/esp-idf --recursive esp-idf-odroidgo &&
+    $python -m pip install --user -r ~/esp/esp-idf-odroidgo/requirements.txt &&
+    ln -s ~/esp/esp-idf-odroidgo/ ~/esp-idf/    
